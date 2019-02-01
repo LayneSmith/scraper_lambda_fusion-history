090c475b5b40c3541065f596dae4d898ef1b54f4
Layne Smith <laynesmith@Groots-MacBook-Pro.local>
2019-01-31 22:15:23 -0600

    Initial commit

---
+++ gethistory.py
@@ -0,0 +1,55 @@
+import requests
+import csv
+from io import StringIO
+from bs4 import BeautifulSoup
+
+def scrapeHistory():
+    """Scrape history from team history feed."""
+
+    payload_array = [['day', 'date', 'year', 'event', 'time', 'home', 'home_score', 'away', 'away_score', 'tier']]
+
+    # GET SCHOOLS LIST
+    url = 'https://home.gotsoccer.com/rankings/team_async.aspx?TeamID=862573&pagesize=100&mode=History'
+    #requests
+    url_r = requests.get(url)
+    #run the requests through soup
+    url_soup = BeautifulSoup(url_r.content, "html.parser")
+
+    dates = url_soup.findAll("p",{"class":"font-weight-bold"})
+    events = url_soup.findAll("p",{"class":"text-smaller"})
+    gameTables = url_soup.findAll("table",{"class":"game-table"})
+
+    for index, game in enumerate(gameTables):   # default is zero
+
+        date_list = dates[index].text.split(', ')
+        game_day = date_list[0].lstrip()
+        game_date = date_list[1]
+        game_year = date_list[2]
+        event = events[index].text
+
+        games = gameTables[index].findAll('tr')
+        for gm in games:
+            tds =  gm.findAll('td')
+            game_time = tds[0].text
+            home_team = tds[1].text
+            away_team = tds[3].text
+            score_list = tds[2].find('span').text.split(' - ')
+            home_score = score_list[0]
+            away_score = score_list[1]
+            game_tier = tds[4].text
+            payload_array.append([game_day, game_date, game_year, event, game_time, home_team, home_score, away_team, away_score, game_tier])
+
+    si = StringIO()
+    cw = csv.writer(si)
+    for row in payload_array:
+        cw.writerow(row)
+
+    # DEPLOY
+    history_data = si.getvalue()
+    return history_data
+            
+
+# this function is just for our testing purposes,
+# just calling the main handler function
+if __name__ == '__main__':
+    scrapeHistory()
---
+++ service.py
@@ -0,0 +1,24 @@
+from utils import upload_data_s3
+# import the filename of your scraper
+import gethistory
+import json
+
+def handler(event, context):
+
+    # Call the function from your imported file
+    data = gethistory.scrapeHistory()
+
+    # Update where the file will be saved
+    bucket_filepath = '2018/fusion/fusion-history.csv'
+
+    # Upload it - JSON
+    # upload_data_s3(data, bucket_filepath, 'json')
+
+    # Upload it - CSV
+    upload_data_s3(data, bucket_filepath, 'csv')
+
+
+# this function is just for our testing purposes,
+# just calling the main handler function
+if __name__ == '__main__':
+    handler(1, 2)
